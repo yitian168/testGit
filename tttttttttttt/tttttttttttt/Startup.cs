c18f903a52445670a5052cdb8f9dd2728b5ecb52
lxr01 <543567769@qq.com>
2018-04-08 11:22:21 +0800

    create

---
+++ tttttttttttt/tttttttttttt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tttttttttttt.Startup))]
+namespace tttttttttttt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
